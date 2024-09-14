70d9a7ae361ac6469db00380f4a1e34665c2b5f2
Durvaldo Gonçalves Marques <durvaldomarques@gmail.com>
2024-09-13 21:54:35 -0300

    Add files via upload

--- introdução-js/script.js
+++ introdução-js/script.js
@@ -12,4 +12,31 @@ function geraImc() {
     const mensagem = `Seu imc é: ${imc}`
 
     document.getElementById('resultado').innerHTML = mensagem
+
+    const cs = classificaImc(imc)
+    const classificacao = `E sua classificacao é : ${cs}`
+    document.getElementById('classificacao').innerHTML = classificacao
 }
+
+function classificaImc(imc) {
+    if (imc < 18.5) {
+        return 'Magreza'
+    } else if (imc <= 24.9) {
+        return 'Normal'
+    } else if (imc <= 29.9) {
+        return 'Sobrepeso'
+    } else if (imc <= 38.9 ) {
+        return 'Obesidade'
+    } else {
+        return 'Obesidade Grave'
+    }
+}
+
+
+
+// ! NÃO
+// != DIFERENTE
+// == IGUAL
+// === IGUAL E DO TIPO
+// || OU
+// && E
